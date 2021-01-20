558a418be63ba1f8623fe44412029bee802aa7f2
Mittal1999 <mittal.jain@somaiya.edu>
2021-01-20 22:07:19 +0530

    remote db connection

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,9 @@
 
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "banking_system");
+// $conn = mysqli_connect("localhost", "root", "", "banking_system");
+
+$conn = mysqli_connect("remotemysql.com", "4ISS3KYSdd", "G7nqbKK27y", "4ISS3KYSdd");
 
 if (!$conn) {
   die("Could not connect to the database due to the following error --> " . mysqli_connect_error());
